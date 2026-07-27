fffd3630196e1c98441acef8c6bdb2012d4f50f9
Abhigyan-singh19 <singhabhigyan922@gmail.com>
2026-07-27 13:16:15 +0530

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -11,7 +11,6 @@ Score""")
 
 
 #========AGENT CODE=========
-import IPython as ip
 import os
 import time
 import langchain
@@ -106,7 +105,10 @@ Always use different styling"""
 final_prompt = prompt + resume_maker_prompt()
 
 user_details = """user details : given below :
-Give Your Details"""
+My name is Abhigyan singh currently studying in 2nd year ip university
+course - bca . very passionate about ai and software development had completed multiple value added courses
+and also done multiple certification code in the field of ai and software development participated in different high level
+hackathons and competition did my schooling from kamal model sr. sec. school 12th percentage is 76 percent my email is singhabhigyan922@gmail.com"""
 
 query = final_prompt + user_details
 
